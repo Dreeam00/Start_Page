4fb3169bb9487ec0cbc8ebf6fdb42af831a0869f
Dreeam00 <Dreeam00@proton.me>
2025-09-20 10:57:15 +0900

    feat: Use cookies instead of localStorage

--- script.js
+++ script.js
@@ -49,8 +49,29 @@ document.addEventListener('DOMContentLoaded', () => {
     ];
 
     // --- Core Functions ---
+    function setCookie(name, value, days) {
+        let expires = "";
+        if (days) {
+            const date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toUTCString();
+        }
+        document.cookie = name + "=" + (value || "") + expires + "; path=/; SameSite=Lax";
+    }
+
+    function getCookie(name) {
+        const nameEQ = name + "=";
+        const ca = document.cookie.split(';');
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
+        return null;
+    }
+
     function saveSettings() {
-        localStorage.setItem('startPageSettings', JSON.stringify(settings));
+        setCookie('startPageSettings', JSON.stringify(settings), 365);
     }
 
     function applySettings() {
@@ -69,15 +90,15 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function saveLinks() {
-        localStorage.setItem('startPageLinks', JSON.stringify(links));
+        setCookie('startPageLinks', JSON.stringify(links), 365);
     }
 
     function loadData() {
-        const storedSettings = localStorage.getItem('startPageSettings');
+        const storedSettings = getCookie('startPageSettings');
         if (storedSettings) {
             Object.assign(settings, JSON.parse(storedSettings));
         }
-        const storedLinks = localStorage.getItem('startPageLinks');
+        const storedLinks = getCookie('startPageLinks');
         links = storedLinks ? JSON.parse(storedLinks) : [...defaultLinks];
         // Ensure old data structure is compatible
         links.forEach(link => {
